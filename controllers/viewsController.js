b2734a09578e15def95a561723e8cfe4756e6924
Manish Salian <manishsalian16@gmail.com>
2021-05-16 19:48:36 +0530

    Implemented Stripe Messages1

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -77,7 +77,7 @@ exports.getMyTours = catchAsync(async(req,res,next)=>{
 
 exports.alerts = (req,res,next)=>{
     const {alert} =req.query;
-    if(alert === booking) res.locals.alert = 'Your booking was successful'
+    if(alert === 'booking') res.locals.alert = 'Your booking was successful'
 
     next()
 }
