8733aabaed3d06f0d26a059108c1fe492ba64425
Manish Salian <manishsalian16@gmail.com>
2021-05-16 19:30:10 +0530

    Implemented Stripe Updated

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -49,7 +49,7 @@ exports.getCheckoutSession = catchAsync(async (req,res,next)=>{
 const creatBookingCheckout = async session =>{
     const tour = session.client_reference_id;
     const user = (await User.findOne({email: session.customer_email})).id
-    const price = session.display_items[0].amount / 100;
+    const price = session.amount_total / 100;
     await Booking.create({tour, user, price})
 
 
@@ -66,7 +66,7 @@ event = stripe.webhooks.constructEvent(req.body, signature, process.env.STRIPE_W
     return res.status(200).send(`Webhook Error:${err.message}`)
 }
 
-if(event.type === 'checkout.session.completed')    creatBookingCheckout(event.data.object)
+if(event.type === 'checkout.session.completed') creatBookingCheckout(event.data.object)
 
 res.status(200).json({recieved:true})
 }
